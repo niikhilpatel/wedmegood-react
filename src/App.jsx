1c0ff7561a69e1d763331eb8fca4ee5fb22e2718
Nikhil Patel <nikhilpatel26150@gmail.com>
2025-01-25 00:44:08 +0530

    Routing enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import Navbar from "./component/Navbar/Navbar"
 import Home from "./component/Home/Home"
+import Footer from "./component/Footer/Footer"
+import About from "./component/About/About"
+import Venues from './component/Venues/Venues'
+import Gallery from './component/Gallery/Gallery'
+import Contact from './component/Contactus/Contact'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
     <Navbar/>
-    <Home/>
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/About" element={<About/>}/> 
+      <Route path="/Venues" element={<Venues/>}/>
+      <Route path="/Gallery" element={<Gallery/>}/>
+      <Route path="/Contact" element={<Contact/>}/>
+
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
     
   )
 }
